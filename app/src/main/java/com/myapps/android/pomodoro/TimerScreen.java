a3d35c7309044bd51dd122eb93c1d26c55fa4224
steric85 <aneemeh@gmail.com>
2016-05-07 14:52:40 +0530

    Added Start countdown timer

--- app/src/main/java/com/myapps/android/pomodoro/TimerScreen.java
+++ app/src/main/java/com/myapps/android/pomodoro/TimerScreen.java
@@ -1,11 +1,13 @@
 package com.myapps.android.pomodoro;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
@@ -13,20 +15,33 @@ import java.util.concurrent.TimeUnit;
 public class TimerScreen extends AppCompatActivity {
 
     int time;
+    private TextView time_left;
+    private ImageButton button1;
+    private ImageButton button2;
+    private  MediaPlayer alarm_song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_screen);
 
+        Typeface face = Typeface.createFromAsset(getAssets(),"font2.ttf");
+        time_left= (TextView) findViewById(R.id.time_left);
+        time_left.setTypeface(face);
+
+        button1=(ImageButton)findViewById(R.id.button1);
+        button2=(ImageButton)findViewById(R.id.button2);
+        alarm_song = MediaPlayer.create(this,R.raw.winter_wonderland);
+
         Intent i = getIntent();
         time = i.getIntExtra("TIME", 0);
-        startTimer();
+
+        time_left.setText(time + ":00");
+
     }
 
-    private void startTimer() {
-        final TextView time_left = (TextView) findViewById(R.id.time_left);
-        time_left.setText(time + ":00");
+    public void startTimer(View view) {
+
         String duration = (String) time_left.getText();
         String parts[] = duration.split(":");
         int minutes = Integer.parseInt(parts[0]);
@@ -35,7 +50,7 @@ public class TimerScreen extends AppCompatActivity {
 
             public void onTick(long millisUntilFinished) {
 
-                time_left.setText(String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                time_left.setText(String.format("%d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
             }
 
             public void onFinish() {
@@ -44,9 +59,5 @@ public class TimerScreen extends AppCompatActivity {
         }.start();
     }
 
-    public void startCountDown(View view){
-        startTimer();
-    }
 }
-
 
